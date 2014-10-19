57b1503a834836e95b8bd9e0c7579128a21eda47
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 12:13:48 -0700

    Lists all primes up too an inputted number
    Note: Due to the nature of the problem dont put stupid big numbers
    unless you want to wait a long time. I have another implementation that
    uses a Sieve but this one is the naieve approach.

---
+++ 22ListPrimes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _22ListPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            printPrimes(num);
+            Console.ReadLine();
+        }
+
+        private static void printPrimes(int num)
+        {
+            List<int> primes = getPrimes(num);
+            foreach(int i in primes)
+            {
+                Console.Write(i + ", ");
+            }
+        }
+
+        private static List<int> getPrimes(int num)
+        {
+            List<int> primes = new List<int>();
+
+            for (int i = 2; i <= num; i++)
+            {
+                if (isPrime(i))
+                {
+                    primes.Add(i);
+                }
+            }
+            return primes;
+        }
+
+        private static bool isPrime(int num)
+        {
+            bool results = true;
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    results = false;
+                }
+            }
+            return results;   
+        }
+    }
+}
